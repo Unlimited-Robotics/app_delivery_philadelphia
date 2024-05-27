180974c4593f025608807f0ba93e8f0f971e0901
Camilo Camacho <camilo@unlimited-robotics.com>
2024-05-27 17:06:03 -0500

    todos1

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
-import argparse
 import re
+# TODO (Camilo): Do we actually need to import argparse?
+import argparse
+
+# TODO (Camilo): Importing RayaApplicationBase twice
 from raya.application_base import RayaApplicationBase
 from raya.application_base import RayaApplicationBase
 from raya.controllers.navigation_controller import NavigationController
@@ -31,7 +34,11 @@ class RayaApplication(RayaApplicationBase):
             )
 
 
+    # TODO (Camilo): Consider using "main" instead of "loop" and remofe
+    # finish app.
     async def loop(self):
+        # TODO (Camilo): Why not using "run_and_await" instead, so you can 
+        # remove the "is_running" poling.
         await self.fsm_main_task.run_in_background()
         
         while self.fsm_main_task.is_running():
@@ -57,9 +64,12 @@ class RayaApplication(RayaApplicationBase):
             tuple_regex = re.compile(r'\(([^)]+)\)')
             tuples = tuple_regex.findall(arg)
             # Convert the extracted tuples from strings to tuples of floats
+            # TODO (Camilo): No more than 79 characters
             tuple_list = [tuple(map(float, item.split(','))) for item in tuples]
             return tuple_list
         except ValueError as e:
+            # TODO (Camilo): Test custom exception to avoid importing argparse
+            # TODO (Camilo): No more than 79 characters
             raise argparse.ArgumentTypeError(f"Invalid format for locations: {arg}. Expected format: (float,float),(float,float),...")
 
 
@@ -68,19 +78,23 @@ class RayaApplication(RayaApplicationBase):
 
     
     def get_arguments(self):
+        # TODO (Camilo): Change to --location1 X.X,X.X --floor1 basement ...
         self.locations = self.get_argument(
                 '--locations',
                 type=self.parse_location_list,
+                # TODO (Camilo): No more than 79 characters
                 help='Locations to deliver the packages, ex: --locations "\(1.0,2.0\),\(3.0,4.0\)"',
                 required=True,
             )
         self.floor = self.get_argument(
                 '--floor',
                 type=self.parse_floor_list,
+                # TODO (Camilo): No more than 79 characters
                 help='Floors to deliver the packages, ex: --floor "floor1,floor2"',
                 required=True,
             )
         
+        # TODO (Camilo): Add identation to the logs/prints.
         self.log.info('App is running with there args:')
         self.log.info(f'Locations: {self.locations}')
         self.log.info(f'Floors: {self.floor}')
