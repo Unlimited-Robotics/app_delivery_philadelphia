b0c8cac868147614423c1f248c93f00d57e4e9ba
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-05-30 15:22:12 -0500

    :fire: removed unused errors

--- src/FMSs/main/errors.py
+++ src/FMSs/main/errors.py
@@ -1,8 +1,2 @@
 ERR_COULD_NOT_LOCALIZE = (1, 'Robot could not get localized')
-ERR_COULD_NOT_NAV_TO_WAREHOUSE = (2, 'Could not navigate to warehouse')
-ERR_COULD_NOT_NAV_TO_WAREHOUSE_EXIT = (3, 'Could not navigate to warehouse exit')
-ERR_COULD_NOT_NAV_TO_CART = (4, 'Could not navigate to cart')
-ERR_COULD_NOT_NAV_TO_DELIVERY_POINT = (5, 'Could not navigate to delivery point')
-ERR_NAV_RETURN_WAREHOUSE_FAILED = (6, 'Could not navigate to warehouse to return the cart')
-
-ERR_NAVIGATION_ABORTED_BY_USER = (7, 'Navigation aborted by user')
+ERR_NAVIGATION_ABORTED_BY_USER = (2, 'Navigation aborted by user')
