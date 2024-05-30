532a325728456b5856da13dc058bf0f569c4f2cc
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-05-30 15:30:41 -0500

    :fire: remove back button to all screens

--- src/static/ui.py
+++ src/static/ui.py
@@ -1,97 +1,111 @@
-from raya.enumerations import UI_ANIMATION_TYPE
-
 # SCREENS
-
 UI_SCREEN_LOCALIZING = {
-        'title':'Starting Application', 
-        'subtitle':'Localizing 🕵',
-    }
+    'title':'Starting Application', 
+    'subtitle':'Localizing 🕵',
+    'back_button_text': '',
+}
 
 UI_SCREEN_NAV_TO_WAREHOUSE = {
-        'title':'Navigating', 
-        'subtitle':'Going to warehouse',
-    }
+    'title':'Navigating', 
+    'subtitle':'Going to warehouse',
+    'back_button_text': '',
+}
 
 UI_SCREEN_ENTERING_TO_WAREHOUSE = {
-        'title':'Navigating', 
-        'subtitle':'Entering warehouse',
-    }
+    'title':'Navigating', 
+    'subtitle':'Entering warehouse',
+    'back_button_text': '',
+}
 
 UI_SCREEN_WAIT_FOR_PACKAGE_LOAD = {
-        'title':'Waiting for package', 
-        'subtitle':'Please load the package and press the chest button to continue',
-    }
+    'title':'Waiting for package', 
+    'subtitle':'Please load the package and press the chest button to continue',
+    'back_button_text': '',
+}
 
 UI_SCREEN_WAIT_FOR_CART_UNLOAD = {
-        'title':'Waiting for Cart Unload', 
-        'subtitle':'Please unload the cart and press the chest button to continue',
-    }
+    'title':'Waiting for Cart Unload', 
+    'subtitle':'Please unload the cart and press the chest button to continue',
+    'back_button_text': '',
+}
 
 UI_SCREEN_WAIT_FOR_DOOR_OPEN = {
-        'title':'Waiting for door to open', 
-        'subtitle':'Please open the door and press the chest button to continue, and step aside',
-    }
+    'title':'Waiting for door to open', 
+    'subtitle':'Please open the door and press the chest button to continue, and step aside',
+    'back_button_text': '',
+}
 
 UI_SCREEN_NAV_TO_WAREHOUSE_EXIT = {
-        'title':'Navigating', 
-        'subtitle':'Navigating to warehouse exit',
-    }
+    'title':'Navigating', 
+    'subtitle':'Navigating to warehouse exit',
+    'back_button_text': '',
+}
 
 UI_SCREEN_LEAVE_WAREHOUSE = {
-        'title':'Navigating', 
-        'subtitle':'Leaving warehouse',
-    }
+    'title':'Navigating', 
+    'subtitle':'Leaving warehouse',
+    'back_button_text': '',
+}
 
 UI_SCREEN_NAV_TO_WAREHOUSE_RETURN = {
-        'title':'Navigating', 
-        'subtitle':'Going back to warehouse',
-    }
+    'title':'Navigating', 
+    'subtitle':'Going back to warehouse',
+    'back_button_text': '',
+}
 
 UI_SCREEN_NAV_TO_PACKAGE_FLOOR = {
-        'title':'Navigating', 
-        'subtitle':'Delivering package',
-    }
+    'title':'Navigating', 
+    'subtitle':'Delivering package',
+    'back_button_text': '',
+}
 
 UI_SCREEN_DELIVERING_ARRIVE = {
-        'title':'Delivering', 
-        'subtitle':'Delivering complete, please confirm pressing the chest button',
-    }
+    'title':'Delivering', 
+    'subtitle':'Delivering complete, please confirm pressing the chest button',
+    'back_button_text': '',
+}
 
 UI_SCREEN_DELIVERING_SUCCESS = {
-        'title':'Delivering Success ✅', 
-        'subtitle':'The package was delivered successfully',
-    }
+    'title':'Delivering Success ✅', 
+    'subtitle':'The package was delivered successfully',
+    'back_button_text': '',
+}
 
 UI_SCREEN_ALL_PACKAGES_DONE = {
-        'title':'Delivering finished', 
-        'subtitle':'The app went through all the packages and parked the cart.',
-    }
+    'title':'Delivering finished', 
+    'subtitle':'The app went through all the packages and parked the cart.',
+    'back_button_text': '',
+}
 
 UI_SCREEN_RELEASE_CART = {
-        'title':'Releasing Cart', 
-        'subtitle':'Please wait while the cart is being released.',
-    }
+    'title':'Releasing Cart', 
+    'subtitle':'Please wait while the cart is being released.',
+    'back_button_text': '',
+}
 
 UI_PACKAGE_NOT_DELIVERED = {
-        'title':'Package not delivered 🚫', 
-        'subtitle':'No confirmation received',
-    }
+    'title':'Package not delivered 🚫', 
+    'subtitle':'No confirmation received',
+    'back_button_text': '',
+}
 
 UI_SCREEN_REQUEST_FOR_HELP = {
-        'title':'Request for Help',
-        'subtitle':'I\'m stuck, contacting the fleet...',
-    }
+    'title':'Request for Help',
+    'subtitle':'I\'m stuck, contacting the fleet...',
+    'back_button_text': '',
+}
 
 UI_SCREEN_WAIT_FOR_HELP_SELECTOR = {
-        'title':'I\'m stuck, please help me, and choose an option',
-        'back_button_text': '',
-        'max_items_shown': 2,
-        'data': [
-                {'id': 1, 'name': 'Abort App 🚫'}, 
-                {'id': 2, 'name': 'Continue 🚶‍♂️'},
-            ]
-    }
+    'title':'I\'m stuck, please help me, and choose an option',
+    'back_button_text': '',
+    'max_items_shown': 2,
+    'data': [
+            {'id': 1, 'name': 'Abort App 🚫'}, 
+            {'id': 2, 'name': 'Continue 🚶‍♂️'},
+        ]
+}
 
 UI_SCREEN_FAILED = {
-        'title':'Failed',
-    }
+    'title':'Failed',
+    'back_button_text': '',
+}
