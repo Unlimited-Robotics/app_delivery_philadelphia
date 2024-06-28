7853a7eb48b4de176ed61ef2c1acf5a01df8278d
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-06-28 11:36:38 -0400

    add bogota_office nav locations

--- src/static/navigation.py
+++ src/static/navigation.py
@@ -89,4 +89,37 @@ NAV[NAV_WAREHOUSE_MAP_NAME] = {
     }
 }
 
+
+# --------------------------------------------------
+#             Bogota_Office__minigary.101
+# --------------------------------------------------
+
+NAV_WAREHOUSE_MAP_NAME = 'Bogota_Office__minigary.101'
+NAV_HOME_POSITION_NAME = 'home'
+
+NAV_WAREHOUSE_ENTRANCE = {
+        'x':        343.0,
+        'y':        430.0,
+        'angle':    66.88, 
+        'pos_unit': POSITION_UNIT.PIXELS, 
+        'ang_unit': ANGLE_UNIT.DEGREES,
+    }
+
+NAV_WAREHOUSE_EXIT = {
+        'x':        367.0,
+        'y':        365.0,
+        'angle':    -105.5634, 
+        'pos_unit': POSITION_UNIT.PIXELS, 
+        'ang_unit': ANGLE_UNIT.DEGREES,
+    }
+
+NAV_CART_POINT = {
+        'x':        453.0,
+        'y':        274.0,
+        'angle':    -2.35, 
+        'pos_unit': POSITION_UNIT.PIXELS, 
+        'ang_unit': ANGLE_UNIT.DEGREES,
+    }
+
+
 set_nav_data('philly_hospital__basement')
