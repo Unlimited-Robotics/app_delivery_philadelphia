32ab93667aea4f8533573c6e65a1d92b178023db
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-07-11 11:25:32 -0400

    refactor ui static file

--- src/static/ui.py
+++ src/static/ui.py
@@ -1,121 +1,117 @@
 # SCREENS
+from raya.enumerations import UI_THEME_TYPE
+
+UI_COMMON_OPTIONS = {
+    'theme': UI_THEME_TYPE.WHITE,
+    'back_button_text': '',
+}
+
+
 UI_SCREEN_LOCALIZING = {
     'title':'Starting Application', 
     'subtitle':'Localizing 🕵',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_NAV_TO_WAREHOUSE = {
     'title':'Navigating', 
     'subtitle':'Going to warehouse',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_ENTERING_TO_WAREHOUSE = {
     'title':'Navigating', 
     'subtitle':'Entering warehouse',
-    'back_button_text': '',
-}
-
-UI_SCREEN_WAIT_FOR_PACKAGE_LOAD = {
-    'title':'Waiting for package', 
-    'subtitle':'Please load the package and press the chest button to continue',
-    'back_button_text': '',
-}
-
-UI_SCREEN_WAIT_FOR_CART_UNLOAD = {
-    'title':'Waiting for Cart Unload', 
-    'subtitle':'Please unload the cart and press the chest button to continue',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_WAIT_FOR_DOOR_OPEN = {
     'title':'Waiting for door to open',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_NAV_TO_WAREHOUSE_EXIT = {
     'title':'Navigating', 
     'subtitle':'Navigating to warehouse exit',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_NAV_TO_HOME = {
     'title':'Navigating', 
     'subtitle':'Navigating to home position',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_LEAVE_WAREHOUSE = {
     'title':'Navigating', 
     'subtitle':'Leaving warehouse',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_NAV_TO_WAREHOUSE_RETURN = {
     'title':'Navigating', 
     'subtitle':'Going back to warehouse',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_NAV_TO_PACKAGE_POINT = {
     'title':'Navigating', 
     'subtitle':'Delivering package',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_DELIVERING_SUCCESS = {
     'title':'Delivering Success ✅', 
     'subtitle':'The package was delivered successfully',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_ALL_PACKAGES_DONE = {
     'title':'Delivering finished', 
     'subtitle':'The app went through all the packages and parked the cart.',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_RELEASE_CART = {
     'title':'Releasing Cart', 
     'subtitle':'Please wait while the cart is being released.',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_PACKAGE_NOT_DELIVERED = {
     'title':'Package deliver failed 🚫', 
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_REQUEST_FOR_HELP = {
     'title':'Request for Help',
     'subtitle':'I\'m stuck, contacting the fleet...',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_WAIT_FOR_HELP_SELECTOR = {
     'title':'I\'m stuck, please help me, and choose an option',
-    'back_button_text': '',
     'max_items_shown': 2,
     'data': [
             {'id': 1, 'name': 'Abort App 🚫'}, 
             {'id': 2, 'name': 'Continue 🚶‍♂️'},
-        ]
+        ],
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_OPTIONS_DELIVERY_ARRIVED = {
     'title':'Deliver complete, please press one of the following options',
-    'back_button_text': '',
     'max_items_shown': 4,
     'data': [
             {'id': 1, 'name': 'My package was delivered successfully'}, 
             {'id': 2, 'name': 'My package is not the one that i ordered'},
             {'id': 3, 'name': 'My package is damaged'},
             {'id': 4, 'name': 'My package is missing'},
-        ]
+        ],
+    **UI_COMMON_OPTIONS
 }
 
 UI_SCREEN_FAILED = {
     'title':'Failed',
-    'back_button_text': '',
+    **UI_COMMON_OPTIONS
 }
