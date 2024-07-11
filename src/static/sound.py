d63d09c0b05535c0f9bc7bdea000cb2d2a0ca9e5
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-07-11 19:07:49 -0400

    add new sounds

--- src/static/sound.py
+++ src/static/sound.py
@@ -13,3 +13,18 @@ SOUNDS_OBSTACLES_DETECTED = [
     SOUND_OBSTACLE_DETECTED_1, 
     SOUND_OBSTACLE_DETECTED_2
 ]
+
+SOUND_OPEN_DOOR_REQUEST = {
+        'path': 'res:Open_door.mp3',
+        'volume': 100,
+    }
+
+SOUND_OPEN_DOOR_THANKS = {
+        'path': 'res:Thank_you_door.mp3',
+        'volume': 100,
+    }
+
+SOUND_FULL_NAME = {
+        'path': 'res:Full_name.mp3',
+        'volume': 100,
+    }
