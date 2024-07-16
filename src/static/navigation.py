05574fd4669499866d7e66acf9fb8a7c8d1323d3
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-07-16 15:05:28 -0400

    chnage on footprint

--- src/static/navigation.py
+++ src/static/navigation.py
@@ -6,10 +6,10 @@ NAVIGATION_TRY_LIMIT = 1
 OBSTACLE_DETECTION_THRESHOLDS = [2, 7]
 
 GARY_FOOTPRINT = [
-    [-0.25,  0.32],
-    [ 0.25,  0.32],
-    [ 0.25, -0.32],
-    [-0.25, -0.32]
+    [-0.25,  0.28],
+    [ 0.25,  0.28],
+    [ 0.25, -0.28],
+    [-0.25, -0.28]
 ]
 
 GARY_FOOTPRINT_BIG_CART = [
