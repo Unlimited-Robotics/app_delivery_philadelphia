146f3d23342902d526d47b294f3de450ffa02926
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-07-18 19:48:48 -0400

    change on initial state

--- src/FMSs/main/states.py
+++ src/FMSs/main/states.py
@@ -34,7 +34,7 @@ STATES.extend(BASE_STATES)
 
 # First state of FSM, if not defined, the FSM starts in the first element of
 # the STATES list
-INITIAL_STATE = 'NAV_TO_WAITING_ELEVATOR'
+INITIAL_STATE = 'SETUP_ACTIONS'
 
 
 # If the FSM falls into one of these states, the execution finishes.
