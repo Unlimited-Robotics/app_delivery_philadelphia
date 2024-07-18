9a4d2e751aa80a5e29faf4a40868e027308a8364
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-07-18 19:28:46 -0400

    :lipstick:

--- src/FMSs/main/states.py
+++ src/FMSs/main/states.py
@@ -4,9 +4,10 @@ from src.FMSs.BaseAppFSM.states import STATES as BASE_STATES
 STATES = [
         'SETUP_ACTIONS',
         'GO_TO_CART_POINT',
+        
         'NAV_TO_WAITING_ELEVATOR',
-
         'NAV_TO_FLOOR',
+
         'NAV_TO_DELIVERY_POINT',
         'NOTIFY_ORDER_ARRIVED',
         
