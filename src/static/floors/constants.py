63f2d5dea2169931a992755f5d9fa18a0bd8e3bd
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-08-08 12:04:34 -0400

    fix multiple behaviors tree

---
+++ src/static/floors/constants.py
@@ -0,0 +1,34 @@
+from raya.enumerations import POSITION_UNIT, ANGLE_UNIT
+import math
+
+NAVIGATION_OPTIONS_WITHOUT_CART = {
+    'options': {
+        'behavior_tree': 'navigate_and_replan_if_needed'
+    },
+    'xy_tolerance': 0.05,
+}
+
+NAVIGATION_OPTIONS_WITH_CART = {
+    'options': {
+        'behavior_tree': 'wait_and_replan_LF'
+    }
+}
+
+def rotate_180(nav_point):
+    if nav_point['ang_unit'] == ANGLE_UNIT.RADIANS:
+        new_angle = nav_point['angle'] + math.pi
+        # Ensure the angle is within the range [-pi, pi]
+        if new_angle > math.pi:
+            new_angle -= 2 * math.pi
+    elif nav_point['ang_unit'] == ANGLE_UNIT.DEGREES:
+        new_angle = nav_point['angle'] + 180
+        # Ensure the angle is within the range [-180, 180]
+        if new_angle > 180:
+            new_angle -= 360
+    else:
+        raise ValueError("Unsupported angle unit")
+    
+    return {
+        **nav_point,
+        'angle': new_angle,
+    }
--- src/static/floors/floor_02.py
+++ src/static/floors/floor_02.py
@@ -1,32 +1,6 @@
+from .constants import *
+
 from raya.enumerations import POSITION_UNIT, ANGLE_UNIT
-
-import math
-
-NAVIGATION_OPTIONS_WITH_CART = {
-    'options': {
-        'behavior_tree': 'wait_and_replan_LF'
-    }
-}
-
-def rotate_180(nav_point):
-    if nav_point['ang_unit'] == ANGLE_UNIT.RADIANS:
-        new_angle = nav_point['angle'] + math.pi
-        # Ensure the angle is within the range [-pi, pi]
-        if new_angle > math.pi:
-            new_angle -= 2 * math.pi
-    elif nav_point['ang_unit'] == ANGLE_UNIT.DEGREES:
-        new_angle = nav_point['angle'] + 180
-        # Ensure the angle is within the range [-180, 180]
-        if new_angle > 180:
-            new_angle -= 360
-    else:
-        raise ValueError("Unsupported angle unit")
-    
-    return {
-        **nav_point,
-        'angle': new_angle,
-    }
-
 
 elevator_1_max = [1503, 1650, 3.039086082060112]
 elevator_1_min = [1548, 1655, 3.0434133891621333]
