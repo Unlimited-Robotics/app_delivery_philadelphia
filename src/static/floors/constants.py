551a0e4601bb482bd4970a6ebcf79fcadb611fae
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-08-14 09:57:27 -0400

    change on bh

--- src/static/floors/constants.py
+++ src/static/floors/constants.py
@@ -10,7 +10,7 @@ NAVIGATION_OPTIONS_WITHOUT_CART = {
 
 NAVIGATION_OPTIONS_WITH_CART = {
     'options': {
-        'behavior_tree': 'nav_with_cart_restricted40'
+        'behavior_tree': 'nav_with_cart_restricted42'
     }
 }
 
