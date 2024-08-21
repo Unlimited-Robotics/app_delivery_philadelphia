20566b58a8d0f91b120bbf3545bf3b5af0993455
Santiago Barragan <santiago@unlimitedrobotics.com>
2024-08-21 14:51:49 -0400

    fix on logs

--- src/PartialsFSM/CommonFSM/helpers.py
+++ src/PartialsFSM/CommonFSM/helpers.py
@@ -4,10 +4,12 @@ if typing.TYPE_CHECKING:
     from . import CommonFSM
 
 import math
+from copy import deepcopy
 
 from raya.logger import RaYaLogger
-from raya.exceptions import RayaFileDoesNotExist, RayaCommandTimeout
 from raya.exceptions import RayaCommandAlreadyRunning
+from raya.exceptions import RayaCommandTimeout
+from raya.exceptions import RayaCommandTimeout
 
 from .constants import DELAY_BEETWEEN_SOUND_LOOP, DURATION_SOUND_LOOP
 from .constants import LEDS_GARY_SPEAKING
@@ -42,28 +44,28 @@ class CommonHelpers():
     async def custom_cancel_sound(self):
         try:
             await self.app.sound.cancel_all_sounds()
-        except Exception:
-            pass
+        except Exception as e:
+            self.log.error(f'cancel_all_sounds exception {type(e)}')
 
 
     async def custom_animation(self, wait=True, **kwargs):
         try:
             await self.app.leds.animation(**kwargs, wait=wait)
-        except Exception:
-            pass
+        except Exception as e:
+            self.log.error(f'animation exception {type(e)}')
 
 
     async def custom_turn_off_leds(self, group = ''):
         if group != '':
             try:
                 await self.app.leds.turn_off_group(group)
-            except Exception:
-                pass
+            except Exception as e:
+                self.log.error(f'turn_off_group exception {type(e)}')
         else:
             try:
                 await self.app.leds.turn_off_all()
-            except Exception:
-                pass
+            except Exception as e:
+                self.log.error(f'turn_off_all exception {type(e)}')
 
 
     def sound_finish_callback(self, code, msg):
@@ -108,21 +110,27 @@ class CommonHelpers():
 
     async def gary_play_audio_predefined(self, 
             audio: dict, 
-            animation_head_leds: dict = LEDS_GARY_SPEAKING,
-            wait: bool = False
+            animation_head_leds: dict = LEDS_GARY_SPEAKING
         ):
+        animation_head_leds = deepcopy(animation_head_leds)
+        audio = deepcopy(audio)
         audio_length = audio.pop('duration', 0)
-        repetitions = math.ceil( audio_length /  DURATION_SOUND_LOOP)
-        self.log.debug('Playing audio')
-        self.log.debug(f'Audio Name: {audio["name"]}')
-        self.log.debug(f'Audio length: {audio_length}')
-        self.log.debug(f'Repetitions: {repetitions}')
+        repetitions = animation_head_leds.pop('repetitions', 1)
+        if repetitions != 0:
+            repetitions = math.ceil( audio_length /  DURATION_SOUND_LOOP)
+        # self.log.debug('Playing audio')
+        # self.log.debug(f'Audio Name: {audio["name"]}')
+        # self.log.debug(f'Audio length: {audio_length}')
+        # self.log.debug(f'Repetitions: {repetitions}')
         
-        await self.app.sound.play_sound(
-            **audio,
-            wait=False,
-            callback_finish=self.sound_finish_callback
-        )
+        try:
+            await self.app.sound.play_sound(
+                **audio,
+                wait=False,
+                callback_finish=self.sound_finish_callback
+            )
+        except Exception:
+            pass
         await self.custom_animation(
             **animation_head_leds,
             repetitions=repetitions,
